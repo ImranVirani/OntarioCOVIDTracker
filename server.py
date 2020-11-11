e25abcb73b70dea43ce8cf416908d3605b40bc56
potatoman16 <virani.imran@icloud.com>
2020-11-11 01:56:12 -0500

    Removed extra files for Flask app as it is unnecessary

--- server.py
+++
@@ -1,8 +0,0 @@
-# Flask Web app that will serve the website that allows an email to be added to the database so that anyone can recieve email updates.
-from flask import Flask
-from flask import render_template
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template("index.html")
